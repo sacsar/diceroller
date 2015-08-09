378b862bdbffdcef93946fea894f4987825a606e
Sebastian Csar <sacsar@gmail.com>
2015-08-08 19:15:13 -0500

    Add alert service and play alert on roll-response

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,8 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, socket) {
+angular.module('myApp.controllers', ['myApp.services']).
+  controller('AppCtrl', function ($scope, socket, alert) {
   
   // Socket listeners
   socket.on('init', function(data){
@@ -12,6 +12,7 @@ angular.module('myApp.controllers', []).
 
   socket.on('roll-response', function(data){
     $scope.rolls.unshift(data.roll)
+    alert.play()
   });
 
   // scope methods
--- public/js/services.js
+++ public/js/services.js
@@ -5,8 +5,18 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  factory('socket', function (socketFactory) {
+var module = angular.module('myApp.services', []);
+
+module.factory('socket', function (socketFactory) {
     return socketFactory();
   }).
   value('version', '0.1');
+
+module.factory('alert', function($rootScope) {
+  return {
+    play: function(){
+      playAlert()
+      console.log("Play alert")
+    }
+  };
+});
